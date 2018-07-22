505b38eee493bf9087fd75e415655c4ea4d5325f
max beizer <max.beizer@gmail.com>
2018-07-22 10:31:53 -0500

    Add part 10, add typespecs and elm format

--- src/Main.elm
+++ src/Main.elm
@@ -5,56 +5,58 @@ import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
 
 
----- MODEL ----
+type Msg
+    = Increment Int
+    | AddCount
+
 
 type alias Model =
     List Int
 
----- UPDATE ----
-
-type Msg
-    = Increment Int
-
-
-
--- update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-      Increment index ->
-        List.indexedMap
-          (\i count ->
-            if i == index then
-              count + 1
-              else
-                count
-            )
-            model
-
-
----- VIEW ----
 
 viewCount : Int -> Int -> Html Msg
 viewCount index count =
-  div [ class "mb-2" ]
-      [ text (toString count)
-      , button
-          [ class "btn btn-primary", onClick (Increment index) ]
-          [ text "+" ]
-    ]
+    div [ class "mb-2" ]
+        [ text (toString count)
+        , button
+            [ class "btn btn-primary ml-2", onClick (Increment index) ]
+            [ text "+" ]
+        ]
+
 
 view : Model -> Html Msg
 view model =
     div [ class "text-center" ]
-        (List.indexedMap viewCount model)
+        [ div [ class "mb-2" ]
+            [ button
+                [ class "btn btn-primary", onClick AddCount ]
+                [ text "Add Count" ]
+            ]
+        , div [] (List.indexedMap viewCount model)
+        ]
 
 
----- PROGRAM ----
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Increment index ->
+            List.indexedMap
+                (\i count ->
+                    if i == index then
+                        count + 1
+                    else
+                        count
+                )
+                model
+
+        AddCount ->
+            model ++ [ 0 ]
 
 
--- main : Program Never Model Msg
+main : Program Never Model Msg
 main =
     beginnerProgram
-        { model = [0,0]
+        { model = [ 0, 0 ]
         , view = view
         , update = update
         }
