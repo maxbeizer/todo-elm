68c44a17041e507bf1609c2ed75ff874e3f4b80f
max beizer <max.beizer@gmail.com>
2018-07-22 10:01:24 -0500

    Add step 8

--- src/Main.elm
+++ src/Main.elm
@@ -10,18 +10,13 @@ import Html.Events exposing (onClick)
 type alias Model =
     Int
 
-
--- init : ( Model, Cmd Msg )
--- init =
---     ( {}, Cmd.none )
-
-
 ---- UPDATE ----
-
 
 type Msg
     = Increment
     | Decrement
+    | Reset
+
 
 
 -- update : Msg -> Model -> ( Model, Cmd Msg )
@@ -29,9 +24,12 @@ update msg model =
     case msg of
       Increment ->
         model + 1
+
       Decrement ->
         model - 1
 
+      Reset ->
+        0
 
 
 ---- VIEW ----
@@ -49,6 +47,9 @@ view model =
                 [ class "btn btn-danger", onClick Decrement ]
                 [ text "-" ]
             ]
+            , button
+            [ class "btn btn-default", onClick Reset ]
+            [ text "Reset" ]
         ]
 
 
