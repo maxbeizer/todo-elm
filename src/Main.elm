4ebea423c08af8ea77bd0684af625fa8004a3e45
max beizer <max.beizer@gmail.com>
2018-07-22 11:09:09 -0500

    Add part 14

--- src/Main.elm
+++ src/Main.elm
@@ -1,23 +1,31 @@
 module Main exposing (..)
 
-import Html exposing (Html, text, div, beginnerProgram, input)
-import Html.Attributes exposing (class, value)
-import Html.Events exposing (onInput)
+import Html exposing (Html, text, div, beginnerProgram, button, input)
+import Html.Attributes exposing (class, value, autofocus)
+import Html.Events exposing (onInput, onClick)
 
 
 type Msg
     = UpdateText String
+    | AddTodo
 
 
 type alias Model =
-    { text : String }
+    { text : String
+    , todos : List String
+    }
 
 
 view : Model -> Html Msg
 view model =
     div [ class "text-center" ]
-        [ input [ onInput UpdateText, value model.text ] []
-        , div [] [ text model.text ]
+        [ input [ onInput UpdateText, value model.text, autofocus True ] []
+        , button [ onClick AddTodo, class "btn btn-primary" ]
+            [ text "Add Todo" ]
+        , div []
+            (List.map (\todo -> div [] [ text todo ])
+                model.todos
+            )
         ]
 
 
@@ -27,11 +35,14 @@ update msg model =
         UpdateText newText ->
             { model | text = newText }
 
+        AddTodo ->
+            { model | text = "", todos = model.todos ++ [ model.text ] }
+
 
 main : Program Never Model Msg
 main =
     beginnerProgram
-        { model = { text = "" }
+        { model = { text = "", todos = [] }
         , view = view
         , update = update
         }
